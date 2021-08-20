c6a8d35df9160e5e8b9e8411bce7f522221ca1bb
smoothkt4951 <smoothkt4951@users.noreply.github.com>
2021-08-20 07:43:08 +0000

    jekyll build from Action abcb1be34b93beee7d54a9b8fec7a9daa42cda2b

---
+++ sw.js
@@ -0,0 +1,73 @@
+const version = '20210820074307';
+const cacheName = `static::${version}`;
+
+const buildContentBlob = () => {
+  return ["/useful-links/general/2018/08/22/hello-world/","/useful-links/","/useful-links/manifest.json","/useful-links/assets/search.json","/useful-links/assets/styles.css","/useful-links/redirects.json","/useful-links/sitemap.xml","/useful-links/robots.txt","/useful-links/feed.xml","", "/useful-links//assets/default-offline-image.png", "/useful-links//assets/scripts/fetch.js"
+  ]
+}
+
+const updateStaticCache = () => {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(buildContentBlob());
+  });
+};
+
+const clearOldCache = () => {
+  return caches.keys().then(keys => {
+    // Remove caches whose name is no longer valid.
+    return Promise.all(
+      keys
+        .filter(key => {
+          return key !== cacheName;
+        })
+        .map(key => {
+          console.log(`Service Worker: removing cache ${key}`);
+          return caches.delete(key);
+        })
+    );
+  });
+};
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    updateStaticCache().then(() => {
+      console.log(`Service Worker: cache updated to version: ${cacheName}`);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clearOldCache());
+});
+
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Only deal with requests from the same domain.
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  // Always fetch non-GET requests from the network.
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Default url returned if page isn't cached
+  let offlineAsset = "/offline/";
+
+  if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+    // If url requested is an image and isn't cached, return default offline image
+    offlineAsset = "/useful-links//assets/default-offline-image.png";
+  }
+
+  // For all urls request image from network, then fallback to cache, then fallback to offline page
+  event.respondWith(
+    fetch(request).catch(async () => {
+      return (await caches.match(request)) || caches.match(offlineAsset);
+    })
+  );
+  return;
+});
